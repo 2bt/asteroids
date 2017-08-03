392beda7e77184db7b9d7256bdc281a4371d9a11
2bt <s8572327@gmail.com>
2017-08-03 09:58:36 +0200

    format

--- src/audio.cpp
+++ src/audio.cpp
@@ -4,12 +4,12 @@
 
 
 void Audio::sound(SoundType type, float panning) {
-	Voice& v = m_voices[m_voice_index];
-	m_voice_index = (m_voice_index + 1) % m_voices.size();
+    Voice& v = m_voices[m_voice_index];
+    m_voice_index = (m_voice_index + 1) % m_voices.size();
 
-	v.state = Voice::ATTACK;
-	v.level = 0;
-	v.pan   = panning - 0.5;
+    v.state = Voice::ATTACK;
+    v.level = 0;
+    v.pan   = panning - 0.5;
     v.type  = type;
 
     switch (type) {
@@ -20,7 +20,6 @@ void Audio::sound(SoundType type, float panning) {
         v.pitch = 14;
         v.sweep = -0.0015;
         v.vol   = 0.5;
-
         break;
     case ST_SMALL_BANG:
     case ST_MEDIUM_BANG:
@@ -47,61 +46,53 @@ void Audio::sound(SoundType type, float panning) {
 }
 
 void Audio::mix_frame(float* frame) {
-
-//	static double t = 0;
-//	frame[0] = frame[1] = std::sin(t * 2 * M_PI) * 0.1;
-//	t += 440.0 / MIX_RATE;
-
-	for (Voice& v : m_voices) {
-		if (v.state == Voice::OFF && v.level < 0.001) continue;
-		if (v.len > 0 && --v.len == 0) v.state = Voice::OFF;
-		switch (v.state) {
-		case Voice::ATTACK:
-			v.level += 0.01;
-			if (v.level >= 1) {
-				v.level = 1;
-				v.state = Voice::HOLD;
-			}
-			break;
-		case Voice::HOLD:
-			v.level *= v.decay;
-			break;
-		default:
-			v.level *= 0.999;
-		}
+    for (Voice& v : m_voices) {
+        if (v.state == Voice::OFF && v.level < 0.001) continue;
+        if (v.len > 0 && --v.len == 0) v.state = Voice::OFF;
+        switch (v.state) {
+        case Voice::ATTACK:
+            v.level += 0.01;
+            if (v.level >= 1) {
+                v.level = 1;
+                v.state = Voice::HOLD;
+            }
+            break;
+        case Voice::HOLD:
+            v.level *= v.decay;
+            break;
+        default:
+            v.level *= 0.999;
+        }
 
         v.pitch += v.sweep;
         float speed = std::exp2(v.pitch / 12) * 440 / MIX_RATE;
         v.pos += speed;
         v.pos -= (int) v.pos;
 
-		float amp;
+        float amp;
         if      (v.wave == Voice::SAW) amp = v.pos * 2 - 1;
         if      (v.wave == Voice::TRI) amp = v.pos < 0.5 ? 4 * v.pos - 1 : 3 - 4 * v.pos;
-		else if (v.wave == Voice::NOISE) {
+        else if (v.wave == Voice::NOISE) {
             if (v.pos < speed) v.noise = rand() * 2.0 / RAND_MAX - 1;
             amp = v.noise;
         }
 
-
-		amp *= v.level * v.vol * 0.5;
-		frame[0] += amp * std::sqrt(0.5 - v.pan);
-		frame[1] += amp * std::sqrt(0.5 + v.pan);
-//		frame[0] += amp;
-//		frame[1] += amp;
-	}
+        amp *= v.level * v.vol * 0.5;
+        frame[0] += amp * std::sqrt(0.5 - v.pan);
+        frame[1] += amp * std::sqrt(0.5 + v.pan);
+    }
 }
 
 
 void Audio::mix(float* buffer) {
-	for (int i = 0; i < BUFFER_LEN; ++i) {
-		buffer[0] = 0;
-		buffer[1] = 0;
-		mix_frame(buffer);
-		buffer[0] = std::max(-1.0f, std::min(1.0f, buffer[0]));
-		buffer[1] = std::max(-1.0f, std::min(1.0f, buffer[1]));
-		buffer += 2;
-	}
+    for (int i = 0; i < BUFFER_LEN; ++i) {
+        buffer[0] = 0;
+        buffer[1] = 0;
+        mix_frame(buffer);
+        buffer[0] = std::max(-1.0f, std::min(1.0f, buffer[0]));
+        buffer[1] = std::max(-1.0f, std::min(1.0f, buffer[1]));
+        buffer += 2;
+    }
 }
 
 Audio audio;
